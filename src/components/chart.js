f138e1eb54239dbe744ee569a280f969bed8cbd2
derksson <mkraemer000@gmail.com>
2020-05-12 01:06:30 +0200

    chart style update

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,16 +4,25 @@ import LineChart from "react-apexcharts";
 const Chart = () => {
   const [ options, setOptions ] = useState({
     chart: {
-      id: "basic-bar"
+      id: "basic-line"
     },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+    colors: ['#2A4365'],
+    grid: {
+      borderColor: 'rgba(0, 0, 0, .5)'
+    },
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
+      colors: undefined,
+      width: 2,
+      dashArray: 0,      
     }
   });
   const [ series, setSeries ] = useState([
     {
       name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91]
+      data: [30, 40, 45, 50, 49, 60, 70, 91, 50, 49, 60, 70, 91, 30, 40, 45, 50, 49, 60, 70, 91, 50, 49, 60, 70, 91]
     }
   ]);
 
@@ -23,7 +32,7 @@ const Chart = () => {
       <LineChart
         options={options}
         series={series}
-        type="bar"
+        type="line"
       />
       </div>
     </div>
