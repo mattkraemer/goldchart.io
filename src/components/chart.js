b3cdbca98bb0773fae0eafc45f9b2ba4e197b5c6
derksson <mkraemer000@gmail.com>
2020-05-12 15:17:24 +0200

    install & import chart.js

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,9 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect } from "react"
+import BarChart from 'chart.js';
 
 const Chart = () => {
+  const chartRef = React.createRef();
+  
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext("2d");
+        
+    new BarChart(myChartRef, {
+        type: "line",
+        data: {
+            //Bring in data
+            labels: ["Jan", "Feb", "March"],
+            datasets: [
+                {
+                    label: "Sales",
+                    data: [86, 67, 91],
+                }
+            ]
+        },
+        options: {
+            //Customize chart options
+        }
+    });
+  }, [])
+
   return (
     <div style={{zIndex: "-10"}} className="absolute top-0 bottom-0 right-0 left-0">
-
+      <canvas
+          id="myChart"
+          ref={chartRef}
+      />
     </div>
   )
 }
