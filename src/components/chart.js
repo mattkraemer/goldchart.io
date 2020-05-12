c5919529b3af3004a5fff3f7fed995d7521f1d95
derksson <mkraemer000@gmail.com>
2020-05-12 16:13:28 +0200

    chart style update

--- src/components/chart.js
+++ src/components/chart.js
@@ -8,20 +8,42 @@ const Chart = () => {
     const myChartRef = chartRef.current.getContext("2d");
         
     new BarChart(myChartRef, {
-        type: "line",
-        data: {
-            //Bring in data
-            labels: ["Jan", "Feb", "March"],
-            datasets: [
-                {
-                    label: "Sales",
-                    data: [86, 67, 91],
-                }
-            ]
-        },
-        options: {
-            //Customize chart options
+      type: "line",
+      data: {
+        //Bring in data
+        labels: ["Jan", "Feb", "March", "Jan", "Feb", "March", "Jan", "Feb", "March"],
+        datasets: [
+          {
+            label: "Sales",
+            data: [86, 67, 91, 86, 67, 91, 91, 86, 67, 91],
+            borderColor: "rgba(0, 0, 0, .15)",
+            backgroundColor: "rgba(0, 0, 0, .05)"
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          xAxes: [{
+            ticks: {
+              fontSize: "16",
+              fontColor: "#CCC", // this here
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .25)"
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              fontSize: "16",
+              fontColor: "#CCC", // this here
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .25)"
+            }
+          }]
         }
+      } 
     });
   }, [])
 
