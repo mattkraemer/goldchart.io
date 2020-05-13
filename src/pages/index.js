476bdbdbb418d613a2cbf172bc4faf2fad39c7ba
derksson <mkraemer000@gmail.com>
2020-05-13 15:38:25 +0200

    index fetch data update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -7,11 +7,28 @@ import SEO from "../components/seo"
 import Chart from "../components/chart"
 
 const IndexPage = () => {
+  const [ labels, setLabels ] = useState();
+  const [ prices, setPrices ] = useState();
+
+  useEffect(() => {
+    fetch ("https://financialmodelingprep.com/api/v3/historical-chart/1min/ZGUSD")
+      .then((res) => res.json())
+      .then((data) => {
+        var labels = jsonfile.jsonarray.historical.map(function(e) {
+          return e.date;
+        });
+        setLabels(labels);
+        var data = jsonfile.jsonarray.historical.map(function(e) {
+          return e.close;
+        });
+        setPrices(data)
+      })
+  }, [])
 
   return (
     <Layout>
       <SEO title="Home" />
-      <Chart />
+      <Chart labels={labels} prices={prices} />
     </Layout>
   )
 }
