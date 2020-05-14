37b90c06dea83a8e3ad43882e1dc224a30f38687
derksson <mkraemer000@gmail.com>
2020-05-14 21:17:21 +0200

    news update

--- src/components/news.js
+++ src/components/news.js
@@ -4,13 +4,15 @@ import NewsJSON from "../../content/news.json"
 const News = () => {
   const [ news, setNews ] = useState();
 
-  useEffect(() => {
-    
-  }, [])
-
   return (
     <div>
-      News
+      <div className="rounded bg-black bg-opacity-25">
+        {
+          NewsJSON.articles.slice(0,10).map((item, index) => (
+            <a href={item.link} target="_blank" className="block px-6 py-4 border-b border-black hover:text-white">{item.title}</a>
+          ))
+        }
+      </div>
     </div>
   )
 }
