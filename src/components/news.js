2fa9adf24009311855cda8fa9d76993cd55d85b7
derksson <mkraemer000@gmail.com>
2020-05-14 21:35:38 +0200

    news update

--- src/components/news.js
+++ src/components/news.js
@@ -9,7 +9,10 @@ const News = () => {
       <div className="rounded-b bg-black bg-opacity-25">
         {
           NewsJSON.articles.slice(0,10).map((item, index) => (
-            <a href={item.link} target="_blank" className="block px-6 py-4 border-b border-gray-900 hover:text-white">{item.title}</a>
+            <a href={item.link} target="_blank" className="block px-6 py-4 border-b border-gray-900 text-gray-500 transform transition duration-200 ease-in-out hover:scale-105 hover:text-white">
+              <span className="text-xs font-thin truncate">{item.date_published} &middot; {item.source.name}</span>
+              <h4 className="font-medium">{item.title}</h4>
+            </a>
           ))
         }
       </div>
