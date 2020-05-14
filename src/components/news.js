b025be8d42bfb893ae6080f7d8560943a5f4d00b
derksson <mkraemer000@gmail.com>
2020-05-14 21:41:42 +0200

    news update & moment.js installed

--- src/components/news.js
+++ src/components/news.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import NewsJSON from "../../content/news.json"
 
+var moment = require('moment');
+
 const News = () => {
   const [ news, setNews ] = useState();
 
@@ -8,9 +10,9 @@ const News = () => {
     <div>
       <div className="rounded-b bg-black bg-opacity-25">
         {
-          NewsJSON.articles.slice(0,10).map((item, index) => (
+          NewsJSON.articles.sort((a,b) => new Date(b.date_published) - new Date(a.date_published)).slice(0,5).map((item, index) => (
             <a href={item.link} target="_blank" className="block px-6 py-4 border-b border-gray-900 text-gray-500 transform transition duration-200 ease-in-out hover:scale-105 hover:text-white">
-              <span className="text-xs font-thin truncate">{item.date_published} &middot; {item.source.name}</span>
+              <span className="text-xs font-thin truncate">{moment(item.date_published).fromNow()} &middot; {item.source.name}</span>
               <h4 className="font-medium">{item.title}</h4>
             </a>
           ))
